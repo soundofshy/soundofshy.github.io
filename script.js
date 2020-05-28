9103a7124d5db8bc59e26cc2f75383e210656eca
soundofshy <65426105+soundofshy@users.noreply.github.com>
2020-05-29 00:55:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/* Get all elements with class="close" */
-var closebtns = document.getElementsByClassName("close");
-var i;
-
-/* Loop through the elements, and hide the parent, when clicked on */
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-  this.parentElement.style.display = 'none';
+$(document).ready(function () {
+  $('#chat-logo, .close').toggleClass('open');
 });
